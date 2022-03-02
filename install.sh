adebf264ac1e326db8f94c0f7d0c59b0937bdf10
Howard Huang <hao@hwrd.me>
2022-03-01 16:25:25 -0800

    Initial commit
    with account configured, as well as defaults for binds and client
    settings.

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $(uname) == "Darwin" ]
+then
+  CONFIG_DIR="${HOME}/Library/Preferences/aerc"
+elif [ $(uname) == "Darwin" ]
+then
+  CONFIG_DIR="${HOME}/.config/aerc"
+else
+  echo "OS $OS isn't supported. Aborting."
+  exit 1
+fi
+
+mkdir -p ${CONFIG_DIR}
+ln -sf "${PWD}/accounts.conf" "${CONFIG_DIR}/accounts.conf"
+ln -sf "${PWD}/aerc.conf" "${CONFIG_DIR}/aerc.conf"
+ln -sf "${PWD}/binds.conf" "${CONFIG_DIR}/binds.conf"
